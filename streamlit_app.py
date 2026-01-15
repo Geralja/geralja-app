87de6e4d9db5770c31f80638dc4b2795dc2b274a
Geralja <cocaiatem@gmail.com>
2026-01-15 14:45:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def calcular_distancia_real(lat1, lon1, lat2, lon2):
     R = 6371
     dlat = math.radians(lat2 - lat1)
     dlon = math.radians(lon2 - lon1)
-    a = math.sin(dlat/2)**2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2)**2
+    a = math.sin(dlat/2)*2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2)*2
     return R * (2 * math.atan2(math.sqrt(a), math.sqrt(1-a)))
 
 @st.cache_data(ttl=600)
